fad72d22d6906fdaf16552ad901cc9147541e8a7
BuMax <max@butenko.in>
2022-12-03 23:06:28 +1000

    Exercise #13 completed

---
+++ ex13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+string numberString = Console.ReadLine();
+
+int len = numberString.Length;
+
+var number = Convert.ToInt32(numberString);
+
+if (len > 2)
+{
+    // смещаем число вправо так, чтобы "третья" цифра находилась на позиции единиц
+    var thirdDigit = (number / (int)(Math.Pow(10, len - 3))) % 10;
+    System.Console.WriteLine("Третья цифра: {0}", thirdDigit);
+
+}
+
+else
+    System.Console.WriteLine("третьей цифры нет");
